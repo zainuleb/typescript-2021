ddf4eec9e9ee01ff63499120e35dbfacb58341bd
Zain Ul Ebad <p166024@nu.edu.pk>
2021-08-31 11:14:24 +0500

    Understanding Types

---
+++ app.ts
@@ -0,0 +1,15 @@
+console.log('Time to get started...');
+
+//Add Number
+const add = (num1:number, num2:number):number =>{
+    return num1+num2;
+}
+
+//Decalre Number variables
+const num1:number = 2;
+const num2:number = 3;
+
+//Driver Code
+const result:number = add(num1,num2);
+
+console.log(result);
