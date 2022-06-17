bf6300a5d1144c320cfe957b0a55473fd4b16b78
zainuleb <p166024@nu.edu.pk>
2022-06-17 16:30:02 +0500

    Implemented Function return types as void and undefined

--- app.ts
+++ app.ts
@@ -1,27 +1,9 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (
-    typeof input1 === 'number' &&
-    typeof input2 === 'number' &&
-    resultConversion === 'as-number'
-  ) {
-    result = input1 + input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combineAges = combine(30, 40, 'as string');
-console.log(combineAges);
+function printResults(num: number): void {
+  console.log(`Result: ${num}`);
+}
 
-const combinedStringAges = combine('30', '40', 'as-number');
-
-const combineNames = combine('Max', 'Anna');
-console.log(combineAges);
+printResults(add(5, 12));
