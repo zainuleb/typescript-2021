0da9291d6d9b577caf0b401af54ffa9f4d3ec8cd
zainuleb <p166024@nu.edu.pk>
2022-06-17 16:10:31 +0500

    Implemented Type Aliases to create your own types

--- app.ts
+++ app.ts
@@ -1,15 +1,27 @@
-function combine(input1: number | string, input2: number) {
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
+  if (
+    typeof input1 === 'number' &&
+    typeof input2 === 'number' &&
+    resultConversion === 'as-number'
+  ) {
     result = input1 + input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  return result;
 }
 
-const combineAges = combine(30, 40);
+const combineAges = combine(30, 40, 'as string');
 console.log(combineAges);
+
+const combinedStringAges = combine('30', '40', 'as-number');
 
 const combineNames = combine('Max', 'Anna');
 console.log(combineAges);
