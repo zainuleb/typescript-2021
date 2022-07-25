8c6fb855ca1496595ec02c7af497f451e5060280
zainuleb <p166024@nu.edu.pk>
2022-07-25 05:50:44 +0500

    type-guards-using-in

--- dist/intersection-types.js
+++ dist/intersection-types.js
@@ -4,3 +4,21 @@ const el = {
     privelleges: ['create-server'],
     startDate: new Date(),
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log(`Name: ${emp.name}`);
+    if ('privelleges' in emp)
+        console.log(`Privelleges: ${emp.privelleges}`);
+    if ('startDate' in emp)
+        console.log(`Start Date: ${emp.startDate}`);
+}
+printEmployeeInformation({
+    name: 'Maaz',
+    privelleges: ['dukan-daar'],
+    startDate: new Date(),
+});
--- src/intersection-types.ts
+++ src/intersection-types.ts
@@ -20,3 +20,24 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log(`Name: ${emp.name}`);
+  if ('privelleges' in emp) console.log(`Privelleges: ${emp.privelleges}`);
+  if ('startDate' in emp) console.log(`Start Date: ${emp.startDate}`);
+}
+
+printEmployeeInformation({
+  name: 'Maaz',
+  privelleges: ['dukan-daar'],
+  startDate: new Date(),
+});
